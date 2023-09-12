518511a5dcbb22db97b6f99484771344c2d64d36
PoeySK <piz115@naver.com>
2023-09-12 20:04:43 +0900

    :lipstick: 그라데이션 글꼴로 수정

--- frontend/src/components/layouts/sideMenu/index.jsx
+++ frontend/src/components/layouts/sideMenu/index.jsx
@@ -3,21 +3,19 @@ import "./index.css";
 import { useSelector } from "react-redux";
 import { TabList } from "../../tabs";
 import { ColorModify } from "../../tabs/color/index";
-import { Gradation } from "../../tabs/gradation/index";
+import { Font } from "../../tabs/font/index";
 
 export const Sidebar = () => {
-  const tabId = useSelector(state => state.tabId);
+  const tabId = useSelector((state) => state.tabId);
 
   return (
     <div className="sidebar">
+      <h2>메뉴</h2>
       <div className="content">
-        <h2>색상</h2>
         <div>
           <TabList />
         </div>
-        <div>
-          {tabId === 'color' ? <ColorModify /> : <Gradation />}
-        </div>
+        <div>{tabId === "color" ? <ColorModify /> : <Font />}</div>
       </div>
     </div>
   );
---
+++ frontend/src/components/tabs/font/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Font = (props) => {
+  return <div>Font</div>;
+};
